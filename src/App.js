33a8ec2f2bc4c9bac061941b9559c994a7362da9
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2021-05-20 06:19:42 +0700

    tabs project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+
+const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
+  // loading default is true
+  const [loading, setLoading] = useState(true);
+  // menampung value
+  const [value, setValue] = useState(0);
+  // empty array and will update by api
+  const [jobs, setJobs] = useState([]);
+
+  const jobsAPI = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    // update empty array
+    setJobs(data);
+    // create be false for loading
+    setLoading(false);
+  };
+
+  // inside to useEffect for passing reference
+  useEffect(() => {
+    jobsAPI();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className='loading'>
+        <h2>mohon tunggu</h2>
+      </div>
+    );
+  }
+
+  // destructuring dan masukan ke jobs dgn index default value (0)
+  const { company, title, duties, dates } = jobs[value];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='section' transition-style='in:square:center'>
+      <div className='title'>
+        <h2>experience work</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        {/* button-container */}
+        <div className='button-container'>
+          {jobs.map((item, index) => {
+            return (
+              <button
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && `active-btn`}`}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        {/* jobs-info */}
+        <div className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='date'>{dates}</p>
+
+          {duties.map((duties, index) => {
+            // use flex
+            return (
+              <div className='job-desc'>
+                <FaAngleDoubleRight></FaAngleDoubleRight>
+                <p>{duties}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div class='more-btn'>
+        <button className='btn-more'>more info </button>
+      </div>
     </div>
   );
 }
